f4df1cb313f58f46d972fcca19276134c6350046
Harsh Verma <r694557@cxloyaltycorp.com>
2023-09-02 12:25:52 +0530

    minor change

--- vars/MPLInit.groovy
+++ vars/MPLInit.groovy
@@ -2,4 +2,5 @@ def call() {
   // Using the MPL library and adding the custom path to find modules
   library('mpl')
   MPLModulesPath('com/company/nestedlib')
+  echo "From MPLInit file call method"
 }
