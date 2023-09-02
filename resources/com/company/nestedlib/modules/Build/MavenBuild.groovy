bcbf32e075df0b2ca08550a706c924d72cbc9f35
Harsh Verma <harshverma04111989@gmail.com>
2023-09-02 13:02:03 +0530

    Update MavenBuild.groovy

--- resources/com/company/nestedlib/modules/Build/MavenBuild.groovy
+++ resources/com/company/nestedlib/modules/Build/MavenBuild.groovy
@@ -4,8 +4,8 @@
 
 echo "It's overridden MavenBuild started - let's run the original one"
 
-sh """echo "Let's do ${CFG.'maven.additional_option' ?: 'something important'} here before the maven build" > testfile.txt"""
-sh """sed -i 's/1.0.0-SNAPSHOT/1.2.0-SNAPSHOT/' pom.xml"""
+// sh """echo "Let's do ${CFG.'maven.additional_option' ?: 'something important'} here before the maven build" > testfile.txt"""
+// sh """sed -i 's/1.0.0-SNAPSHOT/1.2.0-SNAPSHOT/' pom.xml"""
 
 MPLModule('Maven Build', CFG)
 
